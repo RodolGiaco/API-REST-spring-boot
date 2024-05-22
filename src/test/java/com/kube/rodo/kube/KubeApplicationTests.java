0b6b8a47a4940960ea1486b2efe52b01655cb131
rodolfo.giacomodonatto <rodolfo.giacomodonatto@sitrack.com>
2024-05-22 10:16:05 -0300

    start.spring.io

---
+++ src/test/java/com/kube/rodo/kube/KubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kube.rodo.kube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
